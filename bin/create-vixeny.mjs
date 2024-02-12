db6b063e8d1b13e24a53e9722ada0708286e0f5b
mimiMonads <mimi@monads.dev>
2024-02-12 10:12:41 +0000

    to mjs

--- bin/create-vixeny.mjs
+++ bin/create-vixeny.mjs
@@ -3,6 +3,10 @@ import inquirer from "inquirer";
 import { exec } from "node:child_process";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const questions = [
   {
