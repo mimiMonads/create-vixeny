0e75ffa291a799724d5d8a76713dd05cd28c8b58
mimiMonads <mimi@monads.dev>
2024-02-27 22:08:24 +0000

    now fr

--- plugins/jsx/src/plugins/jsx.ts
+++ plugins/jsx/src/plugins/jsx.ts
@@ -3,4 +3,7 @@ import { jsxStaticServer } from "vixeny-prespective";
 import * as Dom from "react-dom/server";
 import * as React from "react";
 
-export default jsxStaticServer(Dom)(React)();
+export default  jsxStaticServer(Dom)(React)({
+    //@ts-ignore
+    root: typeof Deno !== 'undefined' ? 'file://' + Deno.cwd():process.cwd(),
+  })
