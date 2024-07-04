37afeaffa24faaa2d6b2337a957a5b1dba42f2d3
mimiMonads <mimi@monads.dev>
2024-07-04 10:51:34 +0100

    Simplyfing main

--- onlyBackend/bun/main.ts
+++ onlyBackend/bun/main.ts
@@ -1,13 +1,8 @@
-import { vixeny, wrap } from "vixeny";
+
 import root from "./src/paths/root.ts";
-import { globalOptions } from "./src/globalOptions.ts";
 
 Bun.serve({
-  fetch: vixeny(globalOptions)([
-    ...wrap(globalOptions)()
-      .union(root.unwrap())
-      .unwrap(),
-  ]),
+  fetch: root.compose(),
 });
 
-console.log("Server in : " + globalOptions?.indexBase.bind);
+
--- onlyBackend/deno/main.ts
+++ onlyBackend/deno/main.ts
@@ -1,12 +1,6 @@
-import { vixeny, wrap } from "vixeny";
 import root from "./src/paths/root.ts";
-import { globalOptions } from "./src/globalOptions.ts";
 
 Deno.serve(
   { port: 3000 },
-  vixeny(globalOptions)([
-    ...wrap(globalOptions)()
-      .union(root.unwrap())
-      .unwrap(),
-  ]),
+  root.compose(),
 );
