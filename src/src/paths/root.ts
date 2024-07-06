e7943b15df6a5198711e4d28b78cee1bf338a3b4
mimiMonads <mimi@monads.dev>
2024-07-06 09:13:44 +0100

    removing deno main and change it for a generic main

--- src/src/paths/root.ts
+++ src/src/paths/root.ts
@@ -1,8 +1,6 @@
 import { wrap } from "vixeny";
 import { globalOptions } from "../globalOptions.ts";
 
-const path = globalOptions.hasName;
-
 export default wrap(globalOptions)()
   .stdPetition({
     path: "/ping",
--- templates/tsx/views/public/$main.tsx
+++ templates/tsx/views/public/$main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header.tsx"; // Assuming you'll rename Header.jsx to Header.tsx
-import Footer from "../components/Footer.tsx"; // Assuming you'll rename Footer.jsx to Footer.tsx
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx"; 
 
 const MainPage: React.FC = () => (
   <>
